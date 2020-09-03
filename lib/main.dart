729e8a34a26b11fe1ebd81090e466ef9cfb6f8fd
charles.chang <charles.chang@royaltek.com>
2020-09-03 11:53:16 +0800

    add request permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
+class PermissionService {
+  final PermissionHandler _permissionHandler = PermissionHandler();
+
+  Future<bool> _requestPermission(PermissionGroup permission) async {
+    var result = await _permissionHandler.requestPermissions([permission]);
+    if (result[permission] == PermissionStatus.granted) {
+      return true;
+    }
+    return false;
+  }
+
+
+  Future<bool> requestContactsPermission({Function onPermissionDenied}) async {
+    var granted = await _requestPermission(PermissionGroup.contacts);
+    if (!granted) {
+      onPermissionDenied();
+    }
+    return granted;
+  }
+}
 void main() {
   runApp(MyApp());
 }
@@ -15,7 +36,11 @@ class MyApp extends StatelessWidget {
             color: Colors.yellow[300],
             child: Text('Request contacts permission'),
             onPressed: () {
-              print('Permission has been denied');
+              PermissionService().requestContactsPermission(
+                onPermissionDenied: () {
+                  print('Permission has been denied');
+                }
+              );
             },
           ),
         ),
